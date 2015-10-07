9c93bd69f1b897a76065ea5995aff3e4d3d61968
FreedomKnight <fk@unisharp.com>
2015-10-07 10:13:18 +0800

    加上 tag 功能以及列出標籤功能

---
+++ src/Traits/IndependentTaggable.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Unisharp\Taggable\Traits;
+
+use Illuminate\Support\Str;
+
+trait IndependentTaggable
+{
+    public function getTagModelName()
+    {
+        if (isset($this->tagModel)) {
+            return $this->tagModel;
+        } else {
+            return $this->getTagModelNameDefault();
+        }
+    }
+
+    public function getTagModelNameDefault()
+    {
+        return $this->getNamespaceName() . '\\' . config('tagable.suffix', 'TagFor') . $this->getShortName() ;
+    }
+
+    public function getShortName()
+    {
+        $reflect = new \ReflectionClass($this);
+        return $reflect->getShortName();
+    }
+
+    public function getNamespaceName()
+    {
+        $reflect = new \ReflectionClass($this);
+        return $reflect->getNamespaceName();
+    }
+
+    public function tags()
+    {
+        $intermediate_table = $this->intermediate_table ?: Str::snake($this->getShortName()) . '_tags';
+        return $this->belongsToMany($this->getTagModelName(), $intermediate_table, 'article_id', 'tag_id');
+    }
+
+    public function tag()
+    {
+        $args = func_get_args();
+        if (count($args) == 1 && is_array($args)) {
+            $tags = $args[0];
+        } else {
+            $tags = $args;
+        }
+
+        foreach ($tags as $tag) {
+            $this->addTag($tag);
+        }
+    }
+
+    public function addTag($tag_name)
+    {
+        $class = $this->getTagModelName();
+        $tag = $class::where('name', $tag_name)->first();
+        if (empty($tag)) {
+            $tag = $class::create(['name' => $tag_name]);
+            $this->tags()->saveMany([$tag]);
+        }
+
+        if (!$this->tags->contains($tag)) {
+            $this->tags()->attach($tag->id);
+        }
+
+    }
+}
